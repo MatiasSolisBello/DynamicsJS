ce9cff5217d92f80649c9ab8e1442383a1dba453
Matias Solis Bello <51685518+MatiasSolisBello@users.noreply.github.com>
2021-11-05 17:40:36 -0300

    Entidad Capacitaciones

---
+++ capacitacion.js
@@ -0,0 +1,39 @@
+//---------------------------------------------
+//  Setea un campo como oculto a cliente
+//---------------------------------------------
+function HideField(executionContext, Proyecto, Caso) {
+    debugger
+    var formContext = executionContext.getFormContext();
+
+    formContext.getControl("cr544_wit_proyecto").setVisible(false);
+    formContext.getControl("cr544_incident").setVisible(false);
+
+}
+
+//---------------------------------------------
+//  La capacitacion se relaciona a (Proyecto/Caso/No Aplica)
+//---------------------------------------------
+function Relation(executionContext) {
+    debugger
+    var formContext = executionContext.getFormContext();
+
+    var relacion = formContext.getAttribute("cr544_capacitacionrelacion").getValue();
+
+    switch (relacion) {
+        case 860090000:
+            formContext.getControl("cr544_wit_proyecto").setVisible(true);
+            formContext.getControl("cr544_incident").setVisible(false);
+            break;
+        case 860090001:
+            formContext.getControl("cr544_wit_proyecto").setVisible(false);
+            formContext.getControl("cr544_incident").setVisible(true);
+            break;
+        case 860090002:
+            formContext.getControl("cr544_wit_proyecto").setVisible(false);
+            formContext.getControl("cr544_incident").setVisible(false);
+            break;
+        default:
+            break;
+    }
+
+}
